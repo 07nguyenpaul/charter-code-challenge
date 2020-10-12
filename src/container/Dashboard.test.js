89454cfde12037be1a0f44d9ca8d5ed9dcb19878
Paul Nguyen <paul.t.nguyen7@gmail.com>
2020-10-12 09:16:35 -0700

    Add config variables to heroku Add tests

--- src/container/Dashboard.test.js
+++ src/container/Dashboard.test.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { configure, shallow } from 'enzyme';
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-test-renderer';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Provider } from 'react-redux';
-import {spy} from 'sinon';
+// import { Provider } from 'react-redux';
+// import {spy} from 'sinon';
 
-import {Dashboard} from './Dashboard';
+import Dashboard from './Dashboard';
+import RestaurantData from '../service/fetchData';
 import { getMockRestaurants } from '../store/mockRestaurants';
-import configureStore from '../store/configureStore';
 
 configure({ adapter: new Adapter() });
 
 describe('Container: Dashboard ', () => {
+  beforeAll(() => {
+    jest.spyOn(global, 'fetch')
+    .mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve({
+            defaultValue: getMockRestaurants()
+        })
+    }))
+  });
+  afterEach(() => {
+      global.fetch.mockClear();
+  });
+  afterAll(() => {
+      global.fetch.mockRestore();
+  });
+
   const props = {
     restaurants: getMockRestaurants(),
     loading: false,
-    fetchRestaurantData: jest.fn(),
+    currentPage: 1,
+    restaurantsPerPage: 10,
+    setRestaurants: jest.fn(),
+    // renderTableRows: jest.fn(),
+    // renderTableColumns: jest.fn(),
+    // fetchRestaurantData: jest.fn(),
   }
 
   let wrapper;
@@ -26,17 +48,35 @@ describe('Container: Dashboard ', () => {
     );
   });
 
-  it('should call componentDidMount', () => {
-    expect(Dashboard.prototype.componentDidMount.callCount).toEqual(1);
-    expect(wrapper.instance().props.fetchRestaurantData).toHaveBeenCalledTimes(1);
+  it('should render a table skeleton while loading', () => {
+    const props = {
+      restaurants: getMockRestaurants(),
+      loading: true,
+      currentPage: 1,
+      restaurantsPerPage: 10,
+    }
+    const wrapper = mount(<Dashboard {...props}/>);
+    expect(wrapper.find('DataTableSkeleton').length).toBe(1);
   });
 
-  it('should render accordion skeleton when loading', () => {
-    const wrapper = shallow(
-      <Dashboard restaurants={[]} loading={true} fetchRestaurantData={jest.fn()} />
-    );
+  it('should fetch restaurant data with useEffect hook', async () => {
+  });
 
-    expect(wrapper.find('.dashboard').length).toEqual(1);
-    expect(wrapper.find('AccordionSkeleton').length).toEqual(1)
+  it('should render 6 table columns', () => {
+    expect(wrapper.find('table thead tr th').length).toBe(6);
+  });
+
+  it('should render 2 filter dropdowns', () => {
+    expect(wrapper.find('Dropdown').length).toBe(2);
+  });
+
+  xit('should render restaurants', () => {
+    act(() => {
+      // Set restaurant state here
+    });
+  });
+
+  it('should render Pagination', () => {
+    expect(wrapper.find('Pagination').prop("currentPage")).toBe(1);
   });
 });
